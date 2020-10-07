290cf089b17277fb853c70d0218649e0175b79db
Daniel Ohn <ohndaniel@gmail.com>
2020-10-07 15:54:47 -0700

    Update App.js

--- client/src/pages/app/App.js
+++ client/src/pages/app/App.js
@@ -1,8 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./App.css"
 
 function App() {
-  return <div className="App">Hello! We've done it!</div>
+  const [error, setError] = useState()
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    fetch(`https://localhost:3001`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).catch((err) => {
+      setError(err)
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      Testing:{" "}
+      <div>
+        {data && { data }} {error && { error }}
+      </div>
+    </div>
+  )
 }
 
 export default App
